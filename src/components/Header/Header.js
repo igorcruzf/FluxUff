6b313195f4b7d373876c1ee885ff6554f4ba9fcc
Igor <figueiredo_igor@id.uff.br>
2020-05-14 09:52:09 -0300

    passagem por parâmetro agora aceito

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import Box from '@material-ui/core/Box';
 
-import './Header.css'
-
 class Header extends Component {
+    constructor(props){
+        super(props)
+        this.state = props
+    }
+
     render() {
-        return <Box display="flex"  margin="0 auto" justifyContent="center" width='65%'  border={1}>
-        <h1>Bacharelado em Ciência da Computação - 3200 horas</h1>
+        return <Box display="flex" margin="0 auto" justifyContent="center" width='65%' border={1}>
+        <h1>{this.state.classProgram} - {this.state.workload}</h1>
     </Box>
     }
 }
