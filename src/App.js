dd150a01234a3cefdd9ad54f6756c154d1bab47f
fauster <lucasfauster@id.uff.br>
2020-05-14 14:44:50 -0300

    Finalizando card

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <tr>
-        <td><ClassCard /> </td>
+        <td><ClassCard/> </td>
       </tr>
     </div>
   );
--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -3,68 +3,84 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import ContentEditable from 'react-contenteditable';
 
 const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
 });
 
 class ClassCard extends Component {
-    constructor(props){
-        super(props)
-        this.state = props
-    }
+  constructor(props) {
+    super(props)
+    this.state = { text: 'Nome da matéria' }
+  }
 
-    componentDidMount(){
-        function editTitle(){
-          var title = document.getElementById('txt');
-          title.onmouseover = function(){
-            this.title = 'Matéria';;
-          }
-          title.onmouseout = function(){
-            this.title = 'Matéria';
-          }
-          title.onclick = function(){
-            var textoAtual = 'Nome da matéria';
-            var input = '<input type="text" name="1" value="'+textoAtual+'">';
-            this.innerHTML = input;
-            var field = this.firstChild;
-            this.onclick = 'Matéria';
-            this.onmouseover = 'Matéria';
-            field.focus();
-            field.select();
-            field.onblur = function(){
-              if(this.value !== '') this.parentNode.innerHTML = this.value;
-              else this.parentNode.innerHTML = 'Nome da matéria';
-              editTitle();
-            }
+  handleChange = evt => {
+    this.setState({ text: evt.target.value });
+  };
+
+  /*componentDidMount(){
+      function editTitle(){
+        var title = document.getElementById('txt');
+        title.onmouseover = function(){
+          this.title = 'Matéria';;
+        }
+        title.onmouseout = function(){
+          this.title = 'Matéria';
+        }
+        title.onclick = function(){
+          var textoAtual = 'Nome da matéria';
+          var input = '<input type="text" name="1" value="'+textoAtual+'">';
+          this.innerHTML = input;
+          var field = this.firstChild;
+          this.onclick = 'Matéria';
+          this.onmouseover = 'Matéria';
+          field.focus();
+          field.select();
+          field.onblur = function(){
+            if(this.value !== '') this.parentNode.innerHTML = this.value;
+            else this.parentNode.innerHTML = 'Nome da matéria';
+            editTitle();
           }
         }
-        editTitle();
-    }
-    
-    render(){
-      
-      return <Card>
-        <CardContent>
-          <Typography id='txt' variant="h5" component="h2" contentEditable="true">
-            Nome da matéria
-          </Typography>
-        </CardContent>
-      </Card>;
-    }
+      }
+      editTitle();
+  }*/
+
+  /*<Typography id='txt' variant="h5" component="h2" contentEditable="true">
+    Nome da matéria
+  </Typography>*/
+
+  render() {
+
+    return <Card >
+      <CardContent>
+        <div >
+          <ContentEditable
+            width='500px'
+            tagName='h5'
+            html={this.state.text} // innerHTML of the editable div
+            disabled={false} // use true to disable edition
+            onChange={this.handleChange} // handle innerHTML 
+            onBlur={this.sanitize}
+          />
+        </div>
+      </CardContent>
+    </Card>;
+  }
 }
 
 export default ClassCard;
