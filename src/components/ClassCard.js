24a1bf7d1c4c27102f7062cec397ae66a1dd8047
fauster <lucasfauster@id.uff.br>
2020-05-15 15:32:04 -0300

    Card ajustado

--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import ContentEditable from 'react-contenteditable';
 import IconButton from '@material-ui/core/IconButton';
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
- 
-var cardStyle = {
-  display: 'block',
-  width: '100px',
-  transitionDuration: '0.3s',
-  height: '60px',
-  backgroundColor: '#D3D3D3',
-  verticalAlign: 'middle'
-}
 
 class ClassCard extends Component {
   constructor(props) {
     super(props)
     this.id = props.id
-    this.state = { 
-      text: 'Nome da matéria',
+    this.state = {
+      text: 'Matéria',
+      color: '#D3D3D3'
     }
   }
 
@@ -29,25 +21,63 @@ class ClassCard extends Component {
   };
 
   able = () => {
-    document.getElementById(this.id + 'b').style.display = 'none';
-    document.getElementById(this.id + 'c').style.display = 'block';
+    document.getElementById(this.id + 'button-plus').style.display = 'none';
+    document.getElementById(this.id + 'button-minus').style.display = 'block';
+    document.getElementById(this.id + 'content').style.display = 'block';
+    this.setState({color: '#F8F8FF'});
+  };
+
+  disable = () => {
+    document.getElementById(this.id + 'button-plus').style.display = 'block';
+    document.getElementById(this.id + 'button-minus').style.display = 'none';
+    document.getElementById(this.id + 'content').style.display = 'none';
+    this.setState({text: 'Matéria', color: '#D3D3D3'});
   };
 
   render() {
 
-    return <Card style={cardStyle}>
-        <IconButton id={this.id + 'b'} onClick={this.able} style={{display: 'block', margin: '0 auto', }}>
-          <FontAwesomeIcon icon={faPlusCircle}/>
-        </IconButton>
-        <ContentEditable
-          id={this.id + 'c'}
-          style={{display: 'none'}}
-          tagName='h5'
-          html={this.state.text} // innerHTML of the editable div
-          disabled={false} // use true to disable edition
-          onChange={this.handleChange} // handle innerHTML 
-          onBlur={this.sanitize}
-        />
+    return <Card style={{
+      display: 'block',
+      width: '100px',
+      transitionDuration: '0.3s',
+      height: '60px',
+      backgroundColor: this.state.color,
+      }}
+      id={this.id + 'card'}
+      >
+      <IconButton
+        id={this.id + 'button-plus'}
+        onClick={this.able}
+        style={{
+          display: 'block',
+          margin: '0 auto',
+          marginTop: '6%'
+        }}>
+        <FontAwesomeIcon icon={faPlusCircle} />
+      </IconButton>
+      <IconButton
+        id={this.id + 'button-minus'}
+        onClick={this.disable}
+        style={{
+          display: 'none',
+          position: 'absolute',
+        }}
+        size='small'>
+        <FontAwesomeIcon icon={faMinusCircle}/>
+      </IconButton>
+      <ContentEditable
+        id={this.id + 'content'}
+        style={{
+          display: 'none',
+          marginTop: '20%',
+          textAlign: 'center'
+        }}
+        tagName='h5'
+        html={this.state.text} // innerHTML of the editable div
+        disabled={false} // use true to disable edition
+        onChange={this.handleChange} // handle innerHTML 
+        onBlur={this.sanitize}
+      />
     </Card>;
   }
 }
