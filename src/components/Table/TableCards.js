24a8dd8c42f63e6f28c9682eccfcc82c74218993
Igor <figueiredo_igor@id.uff.br>
2020-05-15 11:29:37 -0300

    table versão 0.1 finalizada

--- src/components/Table/TableCards.js
+++ src/components/Table/TableCards.js
@@ -3,24 +3,27 @@ import ClassCard from '../ClassCard.js';
 
 class TableCards extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { sizeRow: 15, sizeCol: 15}
+    }
+
     render() {
+        let rows = [];
+        for (var i = 0; i < this.state.sizeRow; i++) {
+            let rowID = `row${i}`
+            let cell = []
+            for (var idx = 0; idx < this.state.sizeCol; idx++) {
+                let cellID = `cell${i}-${idx}`
+                cell.push(<td key={cellID} id={cellID}><ClassCard /></td>)
+            }
+            rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+        }
         return (
-            <table>
-                <tr>
-                    <td style={{paddingRight: '5%'}}><ClassCard/></td>
-                    <td><ClassCard/></td>
-                    <td><ClassCard/></td>
-                </tr>
-                <tr>
-                    <td><ClassCard/></td>
-                    <td><ClassCard/></td>
-                    <td><ClassCard/></td>
-                </tr>
-                <tr>
-                    <td><ClassCard/></td>
-                    <td><ClassCard/></td>
-                    <td><ClassCard/></td>
-                </tr>
+            <table id="table-of-cards" cellPadding='30%'>
+                <tbody>
+                    {rows}
+                </tbody>
             </table>)
     }
 
