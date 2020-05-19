fb6a4114d0b0b8449b8b96f5cbe8eafec5c0068c
Igor <figueiredo_igor@id.uff.br>
2020-05-19 20:49:09 -0300

    terminando a migração para functions

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Box from '@material-ui/core/Box';
 import ContentEditable from 'react-contenteditable'
 //import { makeStyles } from '@material-ui/core/styles';
@@ -15,71 +15,31 @@ import ContentEditable from 'react-contenteditable'
 
 const titulo = {
     margin: '0 10px 0 10px',
-    width:'100%'
+    width: '100%'
 };
 
-class Header extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { text: this.props.classProgram + ' - ' + this.props.workload }
-    }
+function Header(props) {
 
-    handleChange = evt => {
-        this.setState({ text: evt.target.value });
+    let [text, setText] = useState(props.classProgram + ' - ' + props.workload)
+
+    function handleChange(evt) {
+        setText(evt.target.value);
     };
 
-    render() {
-
-        return <div>
+    return (
+        <div>
             <Box border={1} display='flex' marginBottom='60px' margin='0 auto' justifyContent='center' maxWidth='fit-content' minWidth='20%'>
                 <div style={titulo}>
                     <ContentEditable
                         tagName='h1'
-                        html={this.state.text} // innerHTML of the editable div
+                        html={text} // innerHTML of the editable div
                         disabled={false} // use true to disable edition
-                        onChange={this.handleChange} // handle innerHTML 
-                        onBlur={this.sanitize}
+                        onChange={handleChange} // handle innerHTML 
                     />
                 </div>
             </Box>
         </div>
-    }
-
-    /* componentDidMount() {
-        
-        function editTitle() {
-
-            var title = document.getElementsByTagName('h1')[0];
-            var span = title.firstChild;
-
-            span.onMouseOver = function () {
-                this.title = 'Clique para editar o texto';
-                this.style.background = '#f5f5f5';
-                console.log("to funcionando");
-            }
-            span.onMouseOut = function () {
-                this.title = '';
-                this.style.background = '';
-            }
-            span.onClick = function () {
-                var textoAtual = this.firstChild.nodeValue;
-                var input = '<input type="text" name="1" value="' + textoAtual + '">';
-                this.innerHTML = input;
-                var field = this.firstChild;
-                this.onClick = null;
-                this.onMouseOver = null;
-                field.focus();
-                field.select();
-                field.onblur = function () {
-                    this.parentNode.innerHTML = this.value;
-                    editTitle();
-                }
-            }
-        }
-        editTitle();
-    } */
+    )
 }
-
-
 
 export default Header;
