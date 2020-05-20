55139871ed3f3466295d1e9711a5328aca71bcca
Igor <figueiredo_igor@id.uff.br>
2020-05-20 13:32:22 -0300

    table factory

--- src/components/Table/TableCards.js
+++ src/components/Table/TableCards.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ClassCard from "../ClassCard.js";
+import TableFactory from "../../factory/TableFactory.js";
 
 function TableCards(props) {
   const rows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
@@ -11,29 +11,13 @@ function TableCards(props) {
   }
 
   return (
-    <table id="table-of-cards" cellPadding="30%">
-      <thead>
-        {columns.map((i) => (
-          <th id={`cell${i}`}>{i + 1}</th>
-        ))}
-      </thead>
-      <tbody>
-        {rows.map((i) => (
-          <tr key={i} id={`row${i}`}>
-            {columns.map((j) => (
-              <td
-                className={`cell${i}-${j}`}
-                key={`cell${i}-${j}`}
-                id={`cell${i}-${j}`}
-                onClick={() => onClickHandler(`cell${i}-${j}`)}
-              >
-                <ClassCard id={`cell${i}-${j}`} />
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <TableFactory
+      idsArray={props.idsArray}
+      setIds={props.setIds}
+      rows={rows}
+      columns={columns}
+      onClickHandler={onClickHandler}
+    />
   );
 }
 
---
+++ src/factory/TableFactory.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ClassCard from "../components/ClassCard.js";
+
+function TableFactory(props) {
+  return (
+    <table id="table-of-cards" cellPadding="30%">
+      <thead key={'thead-table-cards'}>
+        <tr key={'tr-head'}>
+          {props.columns.map((i) => (
+            <th id={`cell${i}`}>{i + 1}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody key={'tbody-table-cards'}>
+        {props.rows.map((i) => (
+          <tr key={i} id={`row${i}`}>
+            {props.columns.map((j) => (
+              <td
+                className={`cell${i}-${j}`}
+                key={`cell${i}-${j}`}
+                id={`cell${i}-${j}`}
+                onClick={() => props.onClickHandler(`cell${i}-${j}`)}
+              >
+                <ClassCard key={`card${i}-${j}`} id={`card${i}-${j}`} />
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default TableFactory;
