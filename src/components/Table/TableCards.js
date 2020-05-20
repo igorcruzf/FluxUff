a60e49daf7750070992749ef8064745f3a54e3d6
Igor <figueiredo_igor@id.uff.br>
2020-05-19 21:10:41 -0300

    faltou formatar o tablecards

--- src/components/Table/TableCards.js
+++ src/components/Table/TableCards.js
@@ -1,31 +1,40 @@
-import React from 'react';
-import ClassCard from '../ClassCard.js';
+import React from "react";
+import ClassCard from "../ClassCard.js";
 
 function TableCards(props) {
-    const rows = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14];
-    const columns = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+  const rows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  const columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
-    function onClickHandler(cellID) {
-        let vetor = [...(props.idsArray), cellID];
-        props.setIds(vetor)
-    }
+  function onClickHandler(cellID) {
+    let vetor = [...props.idsArray, cellID];
+    props.setIds(vetor);
+  }
 
-
-    return (
-        <table id="table-of-cards" cellPadding='30%'>
-            <thead>
-                {columns.map((i) => <th id={`cell${i}`}>{i + 1}</th>)}
-            </thead>
-            <tbody>
-                {rows.map((i) =>
-                    <tr key={i} id={`row${i}`}>
-                        {columns.map((j) =>
-                            <td className={`cell${i}-${j}`} key={`cell${i}-${j}`} id={`cell${i}-${j}`} onClick={() => onClickHandler(`cell${i}-${j}`)}>
-                                <ClassCard id={`cell${i}-${j}`} />
-                            </td>)}
-                    </tr>)}
-            </tbody>
-        </table>)
+  return (
+    <table id="table-of-cards" cellPadding="30%">
+      <thead>
+        {columns.map((i) => (
+          <th id={`cell${i}`}>{i + 1}</th>
+        ))}
+      </thead>
+      <tbody>
+        {rows.map((i) => (
+          <tr key={i} id={`row${i}`}>
+            {columns.map((j) => (
+              <td
+                className={`cell${i}-${j}`}
+                key={`cell${i}-${j}`}
+                id={`cell${i}-${j}`}
+                onClick={() => onClickHandler(`cell${i}-${j}`)}
+              >
+                <ClassCard id={`cell${i}-${j}`} />
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default TableCards;
