144b37f84b9c80cb0b00494d0e21225d5c3a5eba
Igor <figueiredo_igor@id.uff.br>
2020-05-26 16:20:53 -0300

    resolvido

--- src/components/CardArea/ClassCard.js
+++ src/components/CardArea/ClassCard.js
@@ -9,28 +9,44 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function ClassCard(props) {
   let [color, setColor] = useState("#D3D3D3");
   let [text, setText] = useState("Matéria");
+  let [displayPlus, setDisplayPlus] = useState("block");
+  let [displayMinus, setDisplayMinus] = useState("none");
+  let [displayContent, setDisplayContent] = useState("none");
+  let [ableFlag, setAbleFlag] = useState(false)
+
+  let [opacity, setOpacity] = useState(0.5);
 
   function handleChange(evt) {
     setText(evt.target.value);
   }
 
   function able() {
-    document.getElementById(props.id + "button-plus").style.display = "none";
-    document.getElementById(props.id + "button-minus").style.display = "block";
-    document.getElementById(props.id + "content").style.display = "block";
+    setDisplayPlus("none");
+    setDisplayMinus("block");
+    setDisplayContent("block");
     setColor("#F8F8FF");
+    setAbleFlag(true);
   }
 
   function disable() {
-    document.getElementById(props.id + "button-plus").style.display = "block";
-    document.getElementById(props.id + "button-minus").style.display = "none";
-    document.getElementById(props.id + "content").style.display = "none";
+    setDisplayPlus("block");
+    setDisplayMinus("none");
+    setDisplayContent("none");
     setColor("#D3D3D3");
     setText("Matéria");
+    setAbleFlag(false);
   }
 
+  function visible() {
+    setOpacity(1);
+  }
+
+  function invisible() {
+    if(!ableFlag)
+      setOpacity(0.5);
+  }
   return (
-    <div className={props.id}>
+    <div className={props.id} onMouseEnter={visible} onMouseLeave={invisible}>
       <Card
         style={{
           display: "block",
@@ -38,6 +54,7 @@ function ClassCard(props) {
           transitionDuration: "0.3s",
           height: "60px",
           backgroundColor: color,
+          opacity: opacity,
         }}
         id={props.id}
       >
@@ -45,7 +62,7 @@ function ClassCard(props) {
           id={props.id + "button-plus"}
           onClick={able}
           style={{
-            display: "block",
+            display: displayPlus,
             margin: "0 auto",
             marginTop: "6%",
           }}
@@ -56,7 +73,7 @@ function ClassCard(props) {
           id={props.id + "button-minus"}
           onClick={disable}
           style={{
-            display: "none",
+            display: displayMinus,
             position: "absolute",
           }}
           size="small"
@@ -66,7 +83,7 @@ function ClassCard(props) {
         <ContentEditable
           id={props.id + "content"}
           style={{
-            display: "none",
+            display: displayContent,
             marginTop: "20%",
             textAlign: "center",
             fontFamily: "Arial",
